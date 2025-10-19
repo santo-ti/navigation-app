a9f314dbd502eaa26847e99c11549a53d7d4b68a
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2025-10-19 20:25:46 -0300

    feat: update Product component to use productId from route parameters

--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -3,11 +3,17 @@ import { View } from "react-native";
 import { Header } from "@/components/Header";
 import { StackRoutesProps } from "@/routes/StackRoustes";
 
+// type RouteParams = StackRoutesProps<"Product">;
+
 export function Product({ navigation, route }: StackRoutesProps<"Product">) {
+  // const { params } = useRoute<RouteParams["route"]>();
+  const productId = route.params?.id;
+
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header
-        title={"Product " + route.params?.id}
+        // title={"Product " + params?.id}
+        title={"Product " + productId}
         icon="arrow-circle-left"
         iconPosition="left"
         handleIconClick={() => navigation.goBack()}
